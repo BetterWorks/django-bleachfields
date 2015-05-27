a6c9a3d8a64643fd1c030fab5e8614fdf5525710
Alex Francis <afrancis@betterworks.com>
2015-05-27 16:01:12 -0700

    small setup formatting change

--- setup.py
+++ setup.py
@@ -12,19 +12,21 @@ install_requires = [
 with open('README.rst') as f:
     readme = f.read()
 
-setup(name='bleachfields',
-      version='1.0',
-      packages=['bleachfields'],
-      license='MIT',
-      description='Tools for bleaching text and JSON of HTML',
-      long_description=readme,
-      author='Alex Francis',
-      author_email='afrancis@betterworks.com',
-      install_requires=install_requires,
-      test_suite='tests',
-      classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2.7',
-      ]
-      )
+
+setup(
+    name='bleachfields',
+    version='1.0',
+    packages=['bleachfields'],
+    license='MIT',
+    description='Tools for bleaching text and JSON of HTML',
+    long_description=readme,
+    author='Alex Francis',
+    author_email='afrancis@betterworks.com',
+    install_requires=install_requires,
+    test_suite='tests',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
