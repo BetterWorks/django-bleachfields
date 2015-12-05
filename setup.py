6112ea8b73d29160221ab323162f01a2e3a0762c
David Stensland <me@terite.com>
2015-12-05 03:16:48 -0500

    bump to django 1.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 install_requires = [
-    'Django>=1.4.3',
+    'Django>=1.8.0',
     'jsonfield>=1.0.3',
     'bleach>=1.4.1',
     'six>=1.9.0'
