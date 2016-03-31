ef8b093415c00d0e03e7dcfbda20c1902b5c1b00
David Stensland <me@terite.com>
2016-03-31 14:03:04 -0700

    use html.unescape in python 3.4+ and fix 2.7 tests

--- bleachfields/bleachfield.py
+++ bleachfields/bleachfield.py
@@ -1,15 +1,22 @@
 import re
+import sys
 
 from bleach import clean
-from six.moves import html_parser
 
 ILLEGAL_CHARACTERS_RE = re.compile(r'[\000-\010]|[\013-\014]|[\016-\037]')
+
+
+# unescape was moved to the html module in python 3.4
+if sys.version_info > (3, 4):
+    from html import unescape
+else:
+    from six.moves import html_parser
+    unescape = html_parser.HTMLParser().unescape
 
 
 class BleachField(object):
 
     # Parser for unescaping HTML entities
-    _parser = html_parser.HTMLParser()
 
     def __init__(self, tags=(), strip=True, *args, **kwargs):
         super(BleachField, self).__init__(*args, **kwargs)
@@ -23,4 +30,5 @@ class BleachField(object):
         text = re.sub(ILLEGAL_CHARACTERS_RE, '', text)
         if '<' in text or '&lt' in text:
             text = clean(text, tags=self.tags, strip=self.strip)
-        return self._parser.unescape(text)
+
+        return unescape(text)
